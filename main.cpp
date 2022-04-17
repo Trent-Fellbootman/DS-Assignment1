4ad00a810ee3558f2e8904d484b88fc0bc2c3556
uebian <bocheng.zou@outlook.com>
2022-04-17 21:17:04 +0800

    Add another test for copy

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ using namespace poly;
 
 int main() {
   std::vector<std::pair<int, int>> items1;
-  /*items1.push_back(std::pair<int, int>(1, 10));
-  items1.push_back(std::pair<int, int>(2, 0));
+  items1.push_back(std::pair<int, int>(1, 10));
+  /*items1.push_back(std::pair<int, int>(2, 0));
   items1.push_back(std::pair<int, int>(78, 3));
   Polynomial<int> p1(items1);
   std::vector<std::pair<int, int>> items2;
