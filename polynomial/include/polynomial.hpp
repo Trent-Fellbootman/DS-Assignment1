0997193b6c14fd984ef4d8d09e117f84ce80b2b0
koala <2713730481@qq.com>
2022-04-17 15:23:01 +0800

    finished calculation implementation

--- polynomial/include/polynomial.hpp
+++ polynomial/include/polynomial.hpp
@@ -1,11 +1,32 @@
+#include <stdint.h>
+
+#pragma once
 namespace poly {
 template <typename T>
 
 class Polynomial {
   struct Node {
     T coefficient;
-    uint32_t exponential;
+    uint32_t exponent;
     Node *next;
   };
+
+  Node *head;
+
+public:
+  uint32_t getLength() { return head->exponent; }
+  T evaluate(T x);
+
+  Polynomial(const Polynomial&) = delete;
+  Polynomial& operator=(const Polynomial&) = delete;
+  Polynomial&& operator=(const Polynomial&&) = default;
+
+  Polynomial& copy() {
+    
+  }
+
+  Polynomial&& operator+(Polynomial& poly);
+  Polynomial&& operator*(T scale);
+  Polynomial&& operator-(Polynomial& poly);
 };
 } // namespace poly
