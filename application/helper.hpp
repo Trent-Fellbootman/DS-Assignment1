13dd096f6a6e7af8570037e15994ec7b36f78781
uebian <bocheng.zou@outlook.com>
2022-04-18 11:58:39 +0800

    Delete unused code

--- application/helper.hpp
+++ application/helper.hpp
@@ -54,39 +54,6 @@ std::string strip(const std::string &original, char ch) {
   return t;
 }
 
-std::vector<std::string> separate(std::string original,
-                                  std::set<char> delimiters) {
-  std::vector<std::string> res;
-
-  int last = 0;
-
-  while (true) {
-    bool found = false;
-    for (char delimiter : delimiters) {
-      int tmp = original.find_first_of(delimiter, last);
-      if (tmp != std::string::npos) {
-        if (last < tmp) {
-          res.push_back(
-              std::string(original.begin() + last, original.begin() + tmp));
-        }
-        res.push_back(
-            std::string(original.begin() + tmp, original.begin() + tmp + 1));
-        last = tmp + 1;
-        found = true;
-      }
-    }
-
-    if (!found) {
-      if (last < original.size()) {
-        res.push_back(std::string(original.begin() + last, original.end()));
-      }
-      break;
-    }
-  }
-
-  return std::move(res);
-}
-
 bool isNum(char c) { return (c >= '0' && c <= '9') || c == '.'; }
 
 int getP(CalcOps op) {
